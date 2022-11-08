50a9fb0503f0a09961cebb8f302e111ad7a75b26
Tosya1 <95938909+Tosya1@users.noreply.github.com>
2022-11-09 00:24:20 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,9 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Person {
+public class Person implements Printable, Writable {
     private String name;
     private String lastName;
     private LocalDate birthDate;
@@ -19,15 +21,15 @@ public class Person {
     public Person(String name, String lastName, Integer id, String birthDate, Boolean isMan) {
         this.name = name;
         this.lastName = lastName;
-        this.birthDate = setBirthDate(birthDate);
+        this.birthDate = formatBirthDate(birthDate);
         this.id = id;
-        this.age = setAge();
+        this.age = calculateAge();
         this.isMan = isMan;
         this.isParent = false;
         this.isMarried = false;
     }
 
-    private LocalDate setBirthDate(String date) {
+    private LocalDate formatBirthDate(String date) {
         try {
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
             LocalDate bd = LocalDate.parse(date, dtf);
@@ -42,7 +44,7 @@ public class Person {
         return birthDate;
     }
 
-    private Integer setAge() {
+    private Integer calculateAge() {
         try {
             return LocalDate.now().compareTo(this.birthDate);
         } catch (Exception e) {
@@ -88,14 +90,14 @@ public class Person {
         return isMarried;
     }
 
-    public void setChildren(Person child, Person parent2) {
+    public void addChild(Person child, Person parent2) {
         this.isParent = true;
         children.add(child);
         child.parents.add(this);
         if (parent2 != null) {
             parent2.isParent = true;
             child.parents.add(parent2);
-            parent2.setChildren(child, null);
+            parent2.addChild(child, null);
         }
     }
 
@@ -115,5 +117,61 @@ public class Person {
     public String toString() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return name + " " + lastName + ", " + age + " лет, дата рождения: " + birthDate.format(dtf);
+    }
+
+    @Override
+    public void printInfo() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        System.out.println(name + " " + lastName + ":\nвозраст: " + age + " лет,\nдата рождения: " + birthDate.format(dtf) + ",");
+        if (isMan) {
+            System.out.println("пол: мужской,");
+        } else {
+            System.out.println("пол: женский,");
+        }
+        
+        if (isMarried && isMan) {
+            System.out.println("семейное положение: женат");
+        } else if (isMarried && !isMan) {
+            System.out.println("семейное положение: замужем");
+        } else if (!isMarried && !isMan) {
+            System.out.println("семейное положение: не замужем");
+        } else {
+            System.out.println("семейное положение: не женат");
+        }
+        
+    }
+
+    public void writeInfo() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        File dirCont = new File("personInfo.txt");
+        try {
+            boolean created = dirCont.createNewFile();
+            if (created) {
+                System.out.printf("Файл %s создан\n", dirCont);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try (FileWriter fr = new FileWriter("personInfo.txt", false)) {
+            fr.write(name + " " + lastName + ":\nвозраст: " + age + " лет,\nдата рождения: " + birthDate.format(dtf) + ",\n");
+            if (isMan) {
+                fr.write("пол: мужской,\n");
+            } else {
+                fr.write("пол: женский,\n");
+            }
+            
+            if (isMarried && isMan) {
+                fr.write("семейное положение: женат");
+            } else if (isMarried && !isMan) {
+                fr.write("семейное положение: замужем");
+            } else if (!isMarried && !isMan) {
+                fr.write("семейное положение: не замужем");
+            } else {
+                fr.write("семейное положение: не женат");
+            }
+            fr.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
