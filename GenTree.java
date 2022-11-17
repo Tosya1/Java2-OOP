9d7e3df9f50fff99589bd57a1f1a01da513dffd3
Tosya1 <95938909+Tosya1@users.noreply.github.com>
2022-11-17 18:48:31 +0200

    Update GenTree.java

--- GenTree.java
+++ GenTree.java
@@ -5,20 +5,21 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class GenTree implements Printable, Writable {
-    private Person person;
-    private TreeMap<String, Set<Person>> family;
+@SuppressWarnings("unchecked")
+public class GenTree <T extends Person> implements Printable, Writable {
+    private T person;
+    private TreeMap<String, Set<T>> family;
 
-    public GenTree(Person person) {
+    public GenTree(T person) {
         this.person = person;
         setFamily();
     }
 
     public void setFamily() {
-        this.family = new TreeMap<String, Set<Person>>();
-        this.family.put("Pодители", this.person.getParents());
+        this.family = new TreeMap<String, Set<T>>();
+        this.family.put("Pодители", (Set<T>) this.person.getParents());
         this.family.put("Муж/Женa", getSpouses());
-        this.family.put("Дети", this.person.getChildren());
+        this.family.put("Дети", (Set<T>) this.person.getChildren());
         this.family.put("Братья и сестры", getSiblings());
         this.family.put("Дедушки и бабушки", getGrandParents());
         this.family.put("Тесть/Теща (Свекр/Свекровь)", getParentsInLaw());
@@ -26,11 +27,11 @@ public class GenTree implements Printable, Writable {
         this.family.put("Внуки", getGrandChildren());
     }
 
-    public Set<Person> getSiblings() {
-        Set<Person> siblings = new HashSet<>();
-        for (Person parent : this.person.getParents()) {
+    public Set<T> getSiblings() {
+        Set<T> siblings = new HashSet<>();
+        for (T parent : (Set<T>)this.person.getParents()) {
             if (parent.getChildren().size() > 1) {
-                for (Person child : parent.getChildren()) {
+                for (T child : (Set<T>)parent.getChildren()) {
                     if (!child.equals(this.person)) {
                         siblings.add(child);
                     }
@@ -40,22 +41,22 @@ public class GenTree implements Printable, Writable {
         return siblings;
     }
 
-    public Set<Person> getGrandParents() {
-        Set<Person> grPrnts = new HashSet<>();
-        for (Person parent : this.person.getParents()) {
-            for (Person grPrnt : parent.getParents()) {
+    public Set<T> getGrandParents() {
+        Set<T> grPrnts = new HashSet<>();
+        for (T parent : (Set<T>)this.person.getParents()) {
+            for (T grPrnt : (Set<T>)parent.getParents()) {
                 grPrnts.add(grPrnt);
             }
         }
         return grPrnts;
     }
 
-    public Set<Person> getGrandChildren() {
-        Set<Person> grChldrn = new HashSet<>();
+    public Set<T> getGrandChildren() {
+        Set<T> grChldrn = new HashSet<>();
         if (this.person.getIsParent() == true) {
-            for (Person child : this.person.getChildren()) {
+            for (T child : (Set<T>)this.person.getChildren()) {
                 if (child.getIsParent() == true) {
-                    for (Person grChild : child.getChildren()) {
+                    for (T grChild : (Set<T>)child.getChildren()) {
                         grChldrn.add(grChild);
                     }
                 }
@@ -64,53 +65,53 @@ public class GenTree implements Printable, Writable {
         return grChldrn;
     }
 
-    public Set<Person> getSpouses() {
-        Set<Person> spouses = new HashSet<>();
+    public Set<T> getSpouses() {
+        Set<T> spouses = new HashSet<>();
         if (this.person.getSpouse() != null) {
-            spouses.add(this.person.getSpouse());
+            spouses.add((T) this.person.getSpouse());
         }
         return spouses;
     }
 
-    public Set<Person> getParentsInLaw() {
+    public Set<T> getParentsInLaw() {
         if (this.person.getSpouse() != null) {
-            return this.person.getSpouse().getParents();
+            return (Set<T>) this.person.getSpouse().getParents();
         } else {
             return null;
         }
     }
 
-    public Set<Person> getChildrenInLaw() {
-        Set<Person> childrenInLaw = new HashSet<>();
+    public Set<T> getChildrenInLaw() {
+        Set<T> childrenInLaw = new HashSet<>();
         if (this.person.getChildren() != null) {
             for (Person child : this.person.getChildren()) {
                 if (child.getSpouse() != null) {
-                    childrenInLaw.add(child.getSpouse());
+                    childrenInLaw.add((T) child.getSpouse());
                 }
             }
         }
         return childrenInLaw;
     }
 
-    public Set<Person> getParents() {
-        return this.person.getParents();
+    public Set<T> getParents() {
+        return (Set<T>) this.person.getParents();
     }
 
-    public Set<Person> getChildren() {
-        return this.person.getChildren();
+    public Set<T> getChildren() {
+        return (Set<T>) this.person.getChildren();
     }
 
-    public Person getPerson() {
+    public T getPerson() {
         return person;
     }
 
     public void printInfo() {
         System.out.println(this.person);
-        for (Map.Entry<String, Set<Person>> item : this.family.entrySet()) {
+        for (Map.Entry<String, Set<T>> item : this.family.entrySet()) {
             if (item.getValue() != null && !item.getValue().isEmpty()) {
                 System.out.println(item.getKey() + ":");
                 int count = 1;
-                for (Person person : item.getValue()) {
+                for (T person : item.getValue()) {
                     System.out.println(count + ") " + person);
                     count++;
                 }
@@ -130,11 +131,11 @@ public class GenTree implements Printable, Writable {
         }
         try (FileWriter fr = new FileWriter("familyInfo.txt", false)) {
             fr.write(this.person.toString() + "\n");
-            for (Map.Entry<String, Set<Person>> item : this.family.entrySet()) {
+            for (Map.Entry<String, Set<T>> item : this.family.entrySet()) {
                 if (item.getValue() != null && !item.getValue().isEmpty()) {
                     fr.write(item.getKey() + ":" + "\n");
                     int count = 1;
-                    for (Person person : item.getValue()) {
+                    for (T person : item.getValue()) {
                         fr.write(count + ") " + person + "\n");
                         count++;
                     }
