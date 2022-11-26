b28159fa076105f773db2cdc96d040b96721c1c2
Tosya1 <95938909+Tosya1@users.noreply.github.com>
2022-11-26 23:33:51 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        Presenter pr = new Presenter();
-        pr.inputInfo();
-        pr.inputInfo();
+        Presenter pr = new Presenter(new MyView());
+        pr.inputInfo(new Person());
+        pr.inputInfo(new Person());
         pr.printUsers();
     }
 }
+
